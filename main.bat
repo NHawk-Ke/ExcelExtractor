f77ed380878e3140c948d4df714d8bfedd338a63
NHawk-Ke <39108244+NHawk-Ke@users.noreply.github.com>
2019-05-10 15:07:27 -0400

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,7 +1,4 @@
 set path=%cd%
-ECHO %path%
-START /WAIT %path%\ExcelToCsv.vbs input.xlsx temp.csv
-START /WAIT %path%\processCSV.vbs 
-START /WAIT %path%\CsvToExcel.vbs
-DEL %path%\temp.csv
-DEL %path%\result.csv
+START /WAIT %path%\ExcelToCsv.vbs input.xlsx input.csv
+START /WAIT %path%\Extract.exe 
+DEL %path%\input.csv
